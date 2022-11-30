604f073c422482860a96d61fd7873974c5c6b169
Asli <asliveenstra@gmail.com>
2022-11-30 23:13:21 +0400

    lab 6

---
+++ SQL-lab-Lesson 2.06.sql
@@ -0,0 +1,37 @@
+-- 1. In the table actor, which are the actors whose last names are not repeated? 
+-- For example if you would sort the data in the table actor by last_name, 
+-- you would see that there is Christian Arkoyd, Kirsten Arkoyd, and Debbie Arkoyd. These three actors have the same last name. 
+-- So we do not want to include this last name in our output. 
+-- Last name "Astaire" is present only one time with actor "Angelina Astaire", hence we would want this in our output list.
+SELECT first_name, last_name FROM actor GROUP BY last_name HAVING COUNT(last_name) <=1;
+
+-- 2. Which last names appear more than once? We would use the same logic as in the previous question 
+-- but this time we want to include the last names of the actors where the last name was present more than once
+SELECT last_name, COUNT(*) AS counts FROM actor GROUP BY last_name HAVING COUNT(last_name) > 1;
+
+-- 3. Using the rental table, find out how many rentals were processed by each employee.
+SELECT staff_id, COUNT(*) AS rental_per_staff FROM rental GROUP BY staff_id;
+-- 
+4. Using the film table, find out how many films were released each year.
+SELECT release_year, COUNT(*) AS number_of_release FROM film GROUP BY release_year;
+
+-- 5. Using the film table, find out for each rating how many films were there.
+SELECT rating, COUNT(*) AS count_by_rating FROM film GROUP BY rating;
+
+-- 6. What is the mean length of the film for each rating type. Round off the average lengths to two decimal places
+SELECT rating, ROUND(AVG(length),2) AS avg_per_rating_type FROM film GROUP BY rating;
+
+-- 7. Which kind of movies (rating) have a mean duration of more than two hours?
+SELECT rating, ROUND(AVG(length),2) AS avg_per_rating_type 
+FROM film 
+GROUP BY rating
+HAVING AVG(length) > 120 ;
+
+-- 8. Rank films by length (filter out the rows that have nulls or 0s in length column). 
+SELECT title, length, 
+RANK () OVER (
+ORDER BY length DESC
+) AS the_rank 
+FROM film
+WHERE length !=0 AND length IS NOT NULL;
+
